5f6ce8686d7de002ba281f2d9c917937d6f838d3
Daniel Wieczorek <danielwieczorek901@gmail.com>
2025-10-28 11:42:12 +0100

    fix(vite.config)

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/HomeSphere',
+  base: '/HomeSphere/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
